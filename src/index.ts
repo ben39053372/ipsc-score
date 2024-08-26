c7f8f94ff9d3bfa6ff7a7305cf6c61c8ded7163f
misvcs99 <misvcs99@gmail.com>
2024-08-26 15:30:11 +0800

    fix: import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { CloudEventFunction, http } from "@google-cloud/functions-framework";
 import { main } from "./functions/main";
+import { getAllMatches } from "./functions/getAllMatches";
 
 export const getScore: CloudEventFunction = async (cloudEvent) => {
   await main();
