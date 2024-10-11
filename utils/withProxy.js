59dc308906fbd662d84da38267e0365f3cfca503
BenCheng <ben39053372@gmail.com>
2024-10-11 10:48:37 +0800

    build

--- utils/withProxy.js
+++ utils/withProxy.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.withProxy = void 0;
 const withProxy = (url, index = 1) => {
-    const proxyUrl = `https://proxy-workers${(index % 10) + 1}.brcs68m5yk.workers.dev/proxy?modify&proxyUrl=${url}`;
+    const proxyUrl = `https://proxy-workers${(index % 10) + 1}.ilovehk-ben.workers.dev/proxy?modify&proxyUrl=${url}`;
     console.log({ proxyUrl });
     return proxyUrl;
 };
