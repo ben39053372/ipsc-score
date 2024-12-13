a9a0f74eef5e4ee68050a7bc89567ffb0d11e599
misvcs99 <misvcs99@gmail.com>
2024-12-13 16:55:32 +0800

    update proxy

--- src/utils/withProxy.ts
+++ src/utils/withProxy.ts
@@ -1,7 +1,15 @@
 export const withProxy = (url: string, index: number = 1) => {
-  const proxyUrl = `https://proxy-workers${
-    (index % 10) + 1
-  }.ilovehk-ben.workers.dev/proxy?modify&proxyUrl=${url}`;
+  let proxyUrl;
+  if (index % 2) {
+    proxyUrl = `https://proxy-workers${
+      (index % 10) + 1
+    }.ilovehk-ben.workers.dev/proxy?modify&proxyUrl=${url}`;
+  } else {
+    proxyUrl = `https://proxy-workers${
+      (index % 10) + 1
+    }.brcs68m5yk.workers.dev/proxy?modify&proxyUrl=${url}`;
+  }
+
   console.log({ proxyUrl });
   return proxyUrl;
 };
