e494e3d702b18b3dc323780afdbe760765d8f15d
BenCheng <ben39053372@gmail.com>
2025-06-28 23:10:23 +0800

    fix: cache control

--- src/utils/uploadJson.ts
+++ src/utils/uploadJson.ts
@@ -4,7 +4,14 @@ export async function uploadJson(name: string, contents: string) {
   const bucketName = "ipsc-score";
   const destFileName = name;
   const storage = new Storage();
-  await storage.bucket(bucketName).file(destFileName).save(contents);
+  await storage
+    .bucket(bucketName)
+    .file(destFileName)
+    .save(contents, {
+      metadata: {
+        "Cache-control": "public, max-age=300",
+      },
+    });
   await storage.bucket(bucketName).file(destFileName).makePublic();
   console.log(
     `${destFileName} with contents ${contents} uploaded to ${bucketName}.`
@@ -15,14 +22,7 @@ export async function uploadMatchJson(contents: string) {
   const bucketName = "ipsc-score-match";
   const destFileName = "matches.json";
   const storage = new Storage();
-  await storage
-    .bucket(bucketName)
-    .file(destFileName)
-    .save(contents, {
-      metadata: {
-        "Cache-control": "public, max-age=300",
-      },
-    });
+  await storage.bucket(bucketName).file(destFileName).save(contents);
   await storage.bucket(bucketName).file(destFileName).makePublic();
   console.log(`matches.json uploaded`);
 }
