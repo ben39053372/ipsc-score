1ab6b79f0c5aec29eaf819872bb0d2f376f032fb
BenCheng <ben39053372@gmail.com>
2026-08-25 01:43:15 +0800

    feat: integrate Firebase services and update project configuration

--- IPSC-Live-Score/src/app/_layout.tsx
+++ IPSC-Live-Score/src/app/_layout.tsx
@@ -1,6 +1,8 @@
 import { Stack } from "expo-router";
+import mobileAds from 'react-native-google-mobile-ads';
 
 export default function RootLayout() {
+  
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Matches" }} />
